6f7ac66188f6846ab0e3f53ec11cd769b422019a
Gustavo Adrien Polli <gapolli@users.noreply.github.com>
2020-03-10 19:45:01 -0300

    Initial code structure uploaded

---
+++ ATE.c
@@ -0,0 +1,27 @@
+/*
+ * Faculdade de Tecnologia da Unicamp - FT
+ *
+ * SI010 - Estrutura de Dados II - 1SEM 2020
+ *
+ * Programadores:
+ * 	Gustavo Adrien Polli	217357
+ * 	Victor Gomes Sampaio	225133
+ *
+ * Programa editor de texto simples com funções de digitar, imprimir na tela
+ * inserir texto, apagar texto, gravar em arquivo e carregar arquivo.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void entrarTexto();
+void imprimir();
+void inserirConteudo(int pos);
+void apagar(int ini, int fim);
+void armazenarEmArquivo(char* nome);
+void carregarDeArquivo();
+
+int main(int argc, char* argv[]){
+
+	return 0;
+}
